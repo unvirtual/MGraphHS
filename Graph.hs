733e5f5002646b3b8752dcc8095057a4b89321d9
Thomas Schutzmeier <tschutzmeier@gmail.com>
2011-11-16 12:12:00 -0500

    Graph: DFS and connectedness

--- Graph.hs
+++ Graph.hs
@@ -10,12 +10,16 @@ module Graph ( Vertex
              , edges
              , vertexBounds
              , adjacency
+             , reachableVertices
+             , isConnected
              , adjVertices
              , degree) where
 
 import Data.Array
 import Control.Monad.State
-
+import Data.Tree
+import Data.List
+import Data.Array
 
 {--------------------------------------------------------------------------------
  -
@@ -80,7 +84,7 @@ vertexBounds gr = (l,h)
 
 -- vertices adjacent to another vertex in a graph
 adjVertices :: Vertex -> UGraph -> [Vertex]
-adjVertices v gr = map (snd) $ filter ((/=) 0 . snd) $ zip verts (adjacency v gr)
+adjVertices v gr = map (fst) $ filter ((/=) 0 . snd) $ zip verts (adjacency v gr)
     where verts = vertices gr
 
 -- adjacency for a vertex in a graph
@@ -92,6 +96,17 @@ adjacency v gr = map (gr!) indices
 -- return the degree of a vertex
 degree :: Vertex -> UGraph -> Int
 degree v = sum . adjacency v
+
+-- return a list of all reachable vertices from a given vertex
+reachableVertices :: UGraph -> Vertex -> [Vertex]
+reachableVertices g v = concatMap preorder (depthFirst g [v])
+
+-- a graph is completely connected, if all vertices are reachable from
+-- an arbitrary start vertex (here we take the first vertex of the
+-- graph)
+isConnected :: UGraph -> Bool
+isConnected gr = length (reachableVertices gr (vv!!0)) == length vv
+    where vv = vertices gr
 
 {--------------------------------------------------------------------------------
  -
@@ -123,6 +138,49 @@ symIx (x,y) | x < y = SymIx (y,x)
 -- returns a row of the upper triangular adjacency matrix of the graph
 row :: Vertex -> UGraph -> [Int]
 row v gr = [x | (SymIx (i,j), x) <- assocs gr, j == v]
+
+{--------------------------------------------------------------------------------
+ -
+ - Depth first search on graphs and related
+ -
+ -------------------------------------------------------------------------------}
+
+type Visited = Array Vertex Bool
+
+-- preorder of a B-tree
+preorder :: Tree a -> [a]
+preorder (Node x xs) = (x:(concatMap preorder xs))
+
+-- postorder of a B-tree
+postorder :: Tree a -> [a]
+postorder (Node x xs) = concatMap postorder xs ++ [x]
+
+-- create a tree from an UGraph given a root vertex. This tree then
+-- contains all reachable vertices from the given vertex and is
+-- infinte in size. It has to be filtered, such that every vertex
+-- appears only once as a node, using depth-first search.
+uGraphToTree :: Vertex -> UGraph -> Tree Vertex
+uGraphToTree v gr = Node v (map (\x -> uGraphToTree x gr) (adjVertices v gr))
+
+-- remove duplicate vertices in a forest of vertex trees
+rmDuplVertices :: Forest Vertex -> State Visited (Forest Vertex)
+rmDuplVertices [] = return []
+rmDuplVertices ((Node v rest):frst) = do
+    visited <- get
+    if not (visited!v) then do
+        modify (\x -> x // [(v,True)])
+        redRest <- rmDuplVertices rest
+        redFrst <- rmDuplVertices frst
+        return ((Node v redRest):redFrst)
+    else do
+        rmDuplVertices frst
+
+-- perform depth-first search on a graph
+depthFirst :: UGraph -> [Vertex] -> Forest Vertex
+depthFirst g v = filterForest bnds (map (flip uGraphToTree g) v) falseArr
+    where filterForest bnds ts = fst . runState (rmDuplVertices ts)
+          falseArr = listArray bnds $ repeat False
+          bnds = vertexBounds g
 
 
 {----------------------------------------------------------------------
