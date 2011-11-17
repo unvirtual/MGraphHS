a4d8b0046545a3caa68ffd1f75ac7f60dd7e1ef8
Thomas Schutzmeier <tschutzmeier@gmail.com>
2011-11-17 16:48:34 -0500

    Graph: added utils for list manipulation

--- Graph.hs
+++ Graph.hs
@@ -355,3 +355,38 @@ appendAtNth :: (Show a) => Int -> [a] -> [[a]] -> [[a]]
 appendAtNth n e list | length list > n = x ++ [(y++e)] ++ xs
                      | otherwise = error $ "appendAtNth: could not append"
     where (x,y:xs) = splitAt n list
+
+-- sort and group a list by the given function
+-- TODO: optimize
+groupSort :: Ord k => (a -> k) -> [a] -> [[a]]
+groupSort f x = map (map snd) $ groupBy fstEq $ sortBy fstCmp $ tpl f x
+    where fstCmp x y = compare (fst x) (fst y)
+          fstEq x y = fst x == fst y
+          tpl f x = map (\y -> (f y, y)) x
+
+-- replace an element in a list given a predicate
+replaceIf :: (a -> Bool) -> a -> [a] -> [a]
+replaceIf f n = map (\x -> if f x then n else x)
+
+-- split the longest sublist of a list of lists and return it together
+-- with the rest
+-- TODO: simplify
+splitLongest :: [[a]] -> ([a], [[a]])
+splitLongest x = (longest, before ++ after)
+    where maxlength = maximum $ map (length) x
+          maxlIx = firstAtWith (\x -> length x == maxlength) x
+          (before, (longest:after)) = splitAt maxlIx x
+
+-- return the index to the first element in a list fulfilling the
+-- predicate
+firstAtWith:: (a -> Bool) -> [a] -> Int
+firstAtWith f x = countUntil f x 0
+    where countUntil f (x:xs) n | f x = n
+                                | otherwise = countUntil f xs (n+1)
+
+-- replace an element in a list with another one
+replaceElem :: (Eq a) => a -> a -> [a] -> [a]
+replaceElem _ _ [] = []
+replaceElem old new (x:xs) | x == old = (new:xs)
+                           | otherwise = [x] ++ replaceElem old new xs
+
