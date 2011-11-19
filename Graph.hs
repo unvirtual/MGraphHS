44b6b6323163959a572d3a3b6a1e382dc76eb0cb
Thomas Schutzmeier <tschutzmeier@gmail.com>
2011-11-19 14:27:47 -0500

    Graph: equitable partition refinement

--- Graph.hs
+++ Graph.hs
@@ -333,17 +333,38 @@ fix = concat . filter isTrivial
 supp :: Partition -> [Vertex]
 supp = concat . filter (not . isTrivial)
 
-refineCell :: UGraph -> Cell -> Cell -> Partition -> (Partition, Partition)
-refineCell gr c1 c2 p
-    | isDiscrete xp = ([c1], p)
-    | otherwise = (xp, neww ++ rest)
-        where xp = splitCell gr c1 c2
-              (maxX, rest) = splitLongest xp
-              neww = replaceElem c1 maxX p
+-- refine a partition with respect to another partition
+refine :: UGraph -> Partition -> [Cell] -> Partition
+refine gr pi [] = pi
+refine gr pi (w:ws) = refine gr pinew wsnew
+    where (pinew, wsnew) = refinePi gr pi w ws
 
--- refine a single cell with respect to another one
-splitCell :: UGraph -> Cell -> Cell -> Partition
-splitCell gr c = groupSort (\x -> cellDegree gr c x)
+-- refine a given current pi elem Pi_G recursively given a w and ws,
+-- return the new pi and the modified ws
+refinePi :: UGraph -> Partition -> Cell -> [Cell] -> (Partition, [Cell])
+refinePi gr [] wi ws = ([], ws)
+refinePi gr pi@(pie:pis) wi ws
+    | isDiscrete pi = (pi, ws)
+    | otherwise = (finalpi, finalws)
+    where finalpi = xx ++ newp
+          (xx, newws) = refinePiE gr pie wi ws
+          (newp, finalws) = refinePi gr pis wi newws
+
+-- Given a cell pie in pi, a list of cells ws and an element w, return
+-- the ordered parition of pie with respect to w and the new list of
+-- cells ws
+refinePiE :: UGraph -> Cell -> Cell -> [Cell] -> (Partition, [Cell])
+refinePiE gr pie wi ws
+    | isUnit xx = ([pie], ws)
+    | otherwise = (xx, wsmod)
+    where xx = orderedPartition gr wi pie
+          (maxX, restX) = splitLongest xx
+          wsmod = (replaceElem pie maxX ws) ++ restX
+
+-- Given a cell W find X = (X_1,..,X_s) which is an element of all partition on a
+-- given cell V such that d(x,W) < d(y,W) for x in X_i and y in X_j if i<j
+orderedPartition :: UGraph -> Cell -> Cell -> Partition
+orderedPartition gr w v = groupSort (\x -> cellDegree gr w x) v
     where -- number of vertices in a cell adjacent to a given vertex
           cellDegree :: UGraph -> Cell -> Vertex -> Int
           cellDegree gr c v = sum $ map fromEnum $ map (isNeighbour gr v) c
